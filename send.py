73e7f9fa3a80dfa5f93e1aa3e85dcebfefb5c20a
DEVL_NOOB <69730462+yunanaexploit@users.noreply.github.com>
2021-07-16 22:13:32 +0800

    Update send.py

--- send.py
+++ send.py
@@ -1,9 +1,11 @@
 import os
 import profil
+import main
 f = open("pesan.txt",'w')
 send = raw_input('isi pesan: ')
 f.write(profil.name)
 f.write(send)
 f.close()
-os.system('curl -T pesan.txt http://ratia.co.za')
+Wokez="curl -T pesan.txt http://"+server+".co.za") 
+os.system(Wokez)
 os.system('python2 send.py')
