8c2c30c8e8be4159e06a5c2a304b9073068ed8e2
DEVL_NOOB <69730462+yunanaexploit@users.noreply.github.com>
2021-07-16 22:19:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
+import sys
 server =
 time = int(input('enter time : '))
 for i in range(time):
-        print '\033[31m'
+        sys.stdout.write('\033[31m') 
         XD ="curl http://"+server+".co.za/pesan.text"
         os.system(XD)
